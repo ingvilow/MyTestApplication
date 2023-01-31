9aecebc3f6e12659e0f8f6ea6dda41611eab35eb
tatyana tatyana <samosh3223@gmail.com>
2023-01-31 16:57:47 +0300

    1. Implementation

--- app/src/main/java/com/example/mytestapplication/domain/ShopItem.kt
+++ app/src/main/java/com/example/mytestapplication/domain/ShopItem.kt
@@ -1,9 +1,14 @@
 package com.example.mytestapplication.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val description: String,
-    val price: Int
+    val price: Int,
+    var id: Int = UNDEFINED,
 ) {
+
+    //hardcoded values should be in companion objects, otherwise it is "magical number"
+    companion object{
+        const val UNDEFINED = -1
+    }
 }
