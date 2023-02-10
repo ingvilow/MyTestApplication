e748edd8518e363d6c99d34e6376a7faa4b4848a
tatyana tatyana <samosh3223@gmail.com>
2023-02-10 14:15:13 +0300

    1. Create second page in layout and ShopItemDetailViewModel() for it

--- app/src/main/java/com/example/mytestapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/presentation/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.layout.second_lpage_layout
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/mytestapplication/presentation/viewModels/ShopItemDetailViewModel.kt
@@ -0,0 +1,89 @@
+package com.example.mytestapplication.presentation.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mytestapplication.data.ShopListRepoImpl
+import com.example.mytestapplication.domain.*
+
+class ShopItemDetailViewModel : ViewModel() {
+
+    private val repository = ShopListRepoImpl
+    private val getOneShopItemById = GetOneShopItemById(repository)
+    private val addShopItemUseCase = AddShopItem(repository)
+    private val editShopItem = EditShopItem(repository)
+
+    val _errorInputName = MutableLiveData<Boolean>()
+    private val _shopItem = MutableLiveData<ShopItem>()
+
+    private val _canClosePage = MutableLiveData<Unit>()
+
+    val canClosePage: LiveData<Unit>
+    get() = _canClosePage
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+
+    fun getShopItem(id: Int) {
+        val item = getOneShopItemById.getOneShopItemById(id)
+        _shopItem.value = item
+    }
+
+
+    fun addShopItem(inputStringName: String, inputDesc: String?) {
+        val name = parsedName(inputStringName)
+        val desc = parsedDesc(inputDesc)
+        val resultInput = validateInput(inputStringName)
+        if (resultInput) {
+            val shopItem = ShopItem(name, desc, 0, true)
+            addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
+        }
+
+    }
+
+    fun editShopItem(inputStringName: String, inputDesc: String?) {
+        val name = parsedName(inputStringName)
+        val desc = parsedDesc(inputDesc)
+        val resultInput = validateInput(inputStringName)
+        if (resultInput) {
+            _shopItem.value?.let {
+                val item = it.copy(name = name, description = desc, 0, true)
+                editShopItem.editShopItem(item)
+                finishWork()
+            }
+
+
+        }
+    }
+
+    //remove whitespaces
+    private fun parsedName(inputStringName: String?): String {
+        return inputStringName?.trim() ?: ""
+    }
+
+    private fun parsedDesc(inputDesc: String?): String {
+        return inputDesc?.trim() ?: ""
+    }
+
+    //check if field is blank
+    private fun validateInput(name: String): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            _errorInputName.value = true
+            result = false
+        }
+        return result
+    }
+
+    private fun resetError() {
+        _errorInputName.value = false
+    }
+    private fun finishWork(){
+        _canClosePage.value = Unit
+    }
+}
